b155bfaa431bd0efc484dd11e0d2e1dbf4019c64
Andreas Hansen <amgh16@student.aau.dk>
2018-02-16 06:48:02 +0100

    import

---
+++ Bmi/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bmi
+{
+    public partial class Form1 : Form
+    {
+        public double bmiResult(double heigth, double weight)
+        {
+            double heightSquare = (heigth * heigth);
+            double bmiResult = (weight / heightSquare);
+            return bmiResult;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) | string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                label3.Text = "Må have input";
+            }
+
+            else
+            {
+                double heigth = double.Parse(textBox1.Text);
+                double weigth = double.Parse(textBox2.Text);
+                double result = bmiResult(heigth, weigth);
+                label3.Text = result.ToString();
+            }
+        }
+    }
+}
